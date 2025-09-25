77b115194e15e08dba0688ec45c920d2f7a87610
Claudio Costa <costa.pederneschi@gmail.com>
2025-09-25 20:55:46 +0000

    atualiza a branch

--- my-nextjs-app/app/components/button.tsx
+++ my-nextjs-app/app/components/button.tsx
@@ -1,4 +1,9 @@
 
 export default function Botao() {
-  return <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Iniciar</button>;
+  function handleClick() {
+    const nome = "Claudio";
+    alert(`Olá, ${nome}!`);
+  }
+  
+    return <button onClick={handleClick} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Iniciar</button>;
 }
