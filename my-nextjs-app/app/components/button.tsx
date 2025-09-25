2df3ba2f23d0c6dc8a57612b39cc17492390eda6
Claudio Costa <costa.pederneschi@gmail.com>
2025-09-25 20:49:07 +0000

    atualiza a branch

---
+++ my-nextjs-app/app/components/button.tsx
@@ -0,0 +1,4 @@
+
+export default function Botao() {
+  return <button>Iniciar</button>;
+}
--- my-nextjs-app/app/page.tsx
+++ my-nextjs-app/app/page.tsx
@@ -1,10 +1,12 @@
 import Titulo from "./components/titulo";
+import Botao from "./components/button";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello, Next.js!</h1>
       <Titulo />
+      <Botao />
     </div>
   );
 }
