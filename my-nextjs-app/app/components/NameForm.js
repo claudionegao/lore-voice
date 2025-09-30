b6111fd1f8bda0ed138fec2e1e74f33605748abb
Claudio Costa <costa.pederneschi@gmail.com>
2025-09-30 18:24:47 -0300

    hahaha again

--- my-nextjs-app/app/components/NameForm.js
+++ my-nextjs-app/app/components/NameForm.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState , useContext } from 'react';
 import { useRouter } from 'next/navigation';
-import {UserContext}   from '../context/UserContext';
+import UserContext   from '../context/UserContext';
 
 const appId = process.env.NEXT_PUBLIC_AGORA_APP_ID;
 
