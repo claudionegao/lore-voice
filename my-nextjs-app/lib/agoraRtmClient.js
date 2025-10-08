37ba094bf7e6eb48904dfc885c713fca0bd148eb
Claudio Costa <costa.pederneschi@gmail.com>
2025-10-08 15:37:04 -0300

    again

--- my-nextjs-app/lib/agoraRtmClient.js
+++ my-nextjs-app/lib/agoraRtmClient.js
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
 
+let AgoraRTM = null;
 let rtmClient = null;
 let rtmChannel = null;
 
+async function loadRTMLibrary() {
+  if (AgoraRTM) return AgoraRTM;
+  if (typeof window === "undefined") return null; // Evita SSR
 
-const [AgoraRTM, setAgoraRTM] = useState(null);
-
-useEffect(() => {
-if (typeof window === "undefined") return;
-
-import("agora-rtm-sdk").then((mod) => setAgoraRTM(mod.default || mod));
-}, []);
+  const mod = await import("agora-rtm-sdk");
+  AgoraRTM = mod.default || mod;
+  return AgoraRTM;
+}
 
 
 export async function createRtmClient(appId, uid, channel, token = null) {
+  await loadRTMLibrary()  
   if (rtmClient) return { rtmClient, rtmChannel };
 
   rtmClient = AgoraRTM.createInstance(appId);
@@ -28,19 +30,23 @@ export async function createRtmClient(appId, uid, channel, token = null) {
 }
 
 export function getRtmClient() {
+  await loadRTMLibrary()  
   return rtmClient;
 }
 
 export function getRtmChannel() {
+  await loadRTMLibrary()  
   return rtmChannel;
 }
 
 export async function sendChannelMessage(message) {
+  await loadRTMLibrary()  
   if (!rtmChannel) return console.warn("Canal RTM não inicializado");
   await rtmChannel.sendMessage({ text: JSON.stringify(message) });
 }
 
 export function onChannelMessage(callback) {
+  await loadRTMLibrary()  
   if (!rtmChannel) return console.warn("Canal RTM não inicializado");
   rtmChannel.on("ChannelMessage", (msg, memberId) => {
     try {
