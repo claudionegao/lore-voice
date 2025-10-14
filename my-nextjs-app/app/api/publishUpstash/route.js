4c3662bd17ab263c91d56d8ff4669bd635cb1df1
Claudio Costa <costa.pederneschi@gmail.com>
2025-10-14 11:29:56 -0300

    hahaha again

--- my-nextjs-app/app/api/publishUpstash/route.js
+++ my-nextjs-app/app/api/publishUpstash/route.js
@@ -7,7 +7,7 @@ const redis = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN
 });
 
-export default async function handler(req, res) {
+export default async function POST(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
   }
