22c1215422c011639679cd1b5be1a59130e98564
Claudio Costa <costa.pederneschi@gmail.com>
2025-10-15 10:31:31 -0300

    console.log

--- my-nextjs-app/app/nome/page.js
+++ my-nextjs-app/app/nome/page.js
@@ -115,10 +115,8 @@ const NomePage = () => {
       if (!_client || !_client.remoteUsers) return;
 
       // Encontra o usuário remoto
-      console.log(_client.remoteUsers);
       const user = _client.remoteUsers.find(u => u._uintid.toString() === targetUid.toString());
       if (!user) return;
-      console.log(user);
       // Verifica se o usuário tem track de áudio
       if (user.audioTrack) {
         if (shouldMute) {
