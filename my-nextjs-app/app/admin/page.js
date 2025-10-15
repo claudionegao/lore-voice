5b288b4210f81908ce35bed945af525d97311743
Claudio Costa <costa.pederneschi@gmail.com>
2025-10-15 15:32:29 -0300

    hahaha again

--- my-nextjs-app/app/admin/page.js
+++ my-nextjs-app/app/admin/page.js
@@ -1,12 +1,9 @@
-"use client";
-
+// app/admin/page.jsx
 export default function AdminPage() {
   return (
-    <div className="relative min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      {/* Fundo escurecido */}
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
       <div className="absolute inset-0 bg-black/70 backdrop-blur-sm"></div>
 
-      {/* Caixa central */}
       <div className="relative bg-gray-800 p-6 rounded-2xl shadow-lg w-80 text-center z-10">
         <h2 className="text-xl mb-4 font-semibold">Digite a senha</h2>
         <input
